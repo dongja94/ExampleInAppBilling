6bed265cdb9c628f8f882bf714a1a2355ed21a33
dongja94 <dongja94@gmail.com>
2015-09-14 10:08:39 +0900

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/begentgroup/inappbillingserver/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.begentgroup.inappbillingserver;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.begentgroup.inappbillingserver";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
